53d8a2a0ecd699beae848b6b21b8c6316a651b4a
Priaranjan <priaranjan53@gmail.com>
2024-08-29 17:25:20 +0530

    Add getComputerChoice() and test it

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello world");
+function getComputerChoice(){
+    let val=Math.random()
+    if(val*3>2) return "Scissors";
+    else if(val*3>1) return "Paper";
+    else return "Rock";
+}
+console.log(getComputerChoice());
