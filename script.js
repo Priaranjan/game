0121a9a6ba79d7cddafe48cec6cd0c2f6e60271f
Priaranjan <priaranjan53@gmail.com>
2024-08-29 17:45:15 +0530

    declare scores and add playRound()

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function getHumanChoice(){
 }
 console.log(getComputerChoice());
 console.log(getHumanChoice());
+let humanScore=0,computerScore=0;
+function playRound(humanChoice,computerChoice){
+    if(humanChoice===computerChoice){
+        console.log("That's a tie");
+    }
+    else if((humanChoice==="Rock" && computerChoice==="Scissors")
+    || (humanChoice==="Paper" && computerChoice==="Rock")||
+    (humanChoice==="Scissors" && computerChoice==="Paper")){
+        humanScore++;
+        console.log("You win !!!");
+    }
+    else{
+        computerScore++;
+    
+    console.log("You lose! "+computerChoice+" beats "+humanChoice);
+    }
+}
