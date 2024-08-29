a1baa5f64736bf6ddbb02723784a7927661a3d78
Priaranjan <priaranjan53@gmail.com>
2024-08-29 17:26:21 +0530

    Add getHumanChoice() and test it

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ function getComputerChoice(){
     else if(val*3>1) return "Paper";
     else return "Rock";
 }
+function getHumanChoice(){
+    console.log("Enter your choice");
+    let choice=prompt("1: Rock, 2: Paper, 3: Scissors");
+    if(choice==1) return "Rock";
+    else if(choice==2) return "Paper";
+    else return "Scissors";
+
+}
 console.log(getComputerChoice());
